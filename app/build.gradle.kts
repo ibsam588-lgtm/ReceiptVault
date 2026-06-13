4f59343c02dc996447e41ba904762e2bab7bf9a1
Sam Shahzad <78374032+Sam5888@users.noreply.github.com>
2026-06-13 16:25:59 -0400

    Upload Play debug symbols

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,9 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
